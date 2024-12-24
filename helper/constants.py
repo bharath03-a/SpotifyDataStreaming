de150bb6b1382ea6d1d7babd2628b03eb66f73eb
Bharath Velamala <bharathvelamala1929@gmail.com>
2024-12-23 17:07:26 -0700

    initial code

---
+++ helper/constants.py
@@ -0,0 +1,11 @@
+# Spotify API 
+TOKEN_URL = "https://accounts.spotify.com/api/token"
+TOKEN_HEADER = {"Content-Type" : "application/x-www-form-urlencoded"}
+TOKEN_REQUEST_BODY = {
+    "grant_type" : "client_credentials",
+    "client_id" : "<client_id>",
+    "client_secret" : "<client_secret>"
+}
+
+CLIENT_ID = "46d7f16926ac4bf4aa0616898def34e8"
+CLIENT_SECRET = "a6320afa973045ae98720f11ef3ef2f4"
